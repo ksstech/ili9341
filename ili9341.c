6cf560c67cab2fa3535f3906f6e68da362e83c01
André Maree <ammaree@users.noreply.github.com>
2022-12-29 12:30:07 +0200

    Move to source file

--- ili9341.c
+++ ili9341.c
@@ -34,10 +34,21 @@
 #define	ili9341FONT_HEIGHT			CONCAT2(ili9341FONT, _HEIGHT)
 #define	ili9341FONT_WIDTH			CONCAT2(ili9341FONT, _WIDTH)
 
+#define	ili9341GPIO_LIGHT			GPIO_NUM_5
+#define	ili9341GPIO_RESET			GPIO_NUM_18
+#define	ili9341GPIO_SCLK			GPIO_NUM_19
+#define	ili9341GPIO_D_C_X			GPIO_NUM_21
+#define ili9341GPIO_CS				GPIO_NUM_22
+#define	ili9341GPIO_MOSI			GPIO_NUM_23
+#define	ili9341GPIO_MISO			GPIO_NUM_25
+
+#define	ili9341LCD_OVERCLOCK		1
+#define ili9341LINES_PARALLEL 		16
+#define	ili9341WR_DSP_BRIGHTNESS	0x51
+
 // ####################################### LCD definitions #########################################
 
 #define	LCD_TYPE_320_240			1		// standard
-
 #define	LCD_TYPE					LCD_TYPE_320_240
 
 #if		(LCD_TYPE == LCD_TYPE_320_240)
@@ -58,6 +69,7 @@
 // ######################################### Enumerations ##########################################
 
 typedef enum { LCD_TYPE_ILI, LCD_TYPE_ST, LCD_TYPE_MAX } type_lcd_t;
+enum { SET_COL_AD = 0x2A, SET_PAG_AD = 0x2B, WR_MEM = 0x2C };
 
 // ########################################## Structures ###########################################
 
--- ili9341.h
+++ ili9341.h
@@ -13,28 +13,10 @@ extern "C" {
 
 // ########################################## Macros ###############################################
 
-#define	ili9341GPIO_LIGHT			GPIO_NUM_5
-#define	ili9341GPIO_RESET			GPIO_NUM_18
-#define	ili9341GPIO_SCLK			GPIO_NUM_19
-#define	ili9341GPIO_D_C_X			GPIO_NUM_21
-#define ili9341GPIO_CS				GPIO_NUM_22
-#define	ili9341GPIO_MOSI			GPIO_NUM_23
-#define	ili9341GPIO_MISO			GPIO_NUM_25
-
-#define	ili9341LCD_OVERCLOCK		1
-
-#define ili9341LINES_PARALLEL 		16
-
-#define	ili9341WR_DSP_BRIGHTNESS	0x51
 #define ENABLE_EPID					0
 
 // ######################################### Enumerations ##########################################
 
-enum {
-	SET_COL_AD	= 0x2A,
-	SET_PAG_AD	= 0x2B,
-	WR_MEM		= 0x2C,
-} ;
 
 // ##################################### GLOBAL variables ##########################################
 
